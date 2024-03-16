bc61d0751d82a6bda57d8432c0c4fe8e9d21f5e3
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-03-16 15:35:42 +0700

    Added LoginScreenView and LoginScreenViewModel
    Added a new class `LoginScreenViewModel` in the `LoginScreenViewModel.cs` file. This class is part of the `QuanLySinhVien.ViewModels.Login` namespace and inherits from `ViewModelBase`. Currently, this class is empty.
    
    In the `LoginScreenView.axaml` file, a new Avalonia window was created with the class `QuanLySinhVien.Views.Login.LoginScreenView`. This window has a design width and height of 800 and 450 respectively, and its title is "LoginScreenView". The window currently only displays the text "Welcome to Avalonia!".
    
    In the `LoginScreenView.axaml.cs` file, a new partial class `LoginScreenView` was created in the `QuanLySinhVien.Views.Login` namespace. This class inherits from `Window` and has a constructor that calls the `InitializeComponent` method. This class is likely to be used to handle events and interactions in the `LoginScreenView` window.

---
+++ QuanLySinhVien/Views/Login/LoginScreenView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace QuanLySinhVien.Views.Login
+{
+    public partial class LoginScreenView : Window
+    {
+        public LoginScreenView()
+        {
+            InitializeComponent();
+        }
+    }
+}
