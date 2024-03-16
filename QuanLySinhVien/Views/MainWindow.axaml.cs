609d7ca05896fc618988726c6c3c5bc8b648217f
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-03-16 15:15:52 +0700

    Title: Updated UI and refactored MainWindow.axaml
    Updated the UI of `WindowsTitleBar.axaml` by changing the background color of the main window to red and making the `TitleBarBackground`, `MinimizeButton`, `MaximizeButton`, and `CloseButton` backgrounds transparent. Refactored `MainWindow.axaml` by reordering namespaces, adding a new `tittlebars` namespace, and including a `WindowsTitleBar` from the `tittlebars` namespace in the `DockPanel`. In `MainWindow.axaml.cs`, added the `Avalonia.Controls.Chrome` namespace, set the `TitleBar` height to 0, and set `ExtendsContentIntoTitleBar` to true in the `MainWindow` constructor.

--- QuanLySinhVien/Views/MainWindow.axaml.cs
+++ QuanLySinhVien/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Controls.Chrome;
 using FluentAvalonia.UI.Windowing;
 
 namespace QuanLySinhVien.Views;
@@ -8,5 +9,7 @@ public partial class MainWindow : AppWindow
     public MainWindow()
     {
         InitializeComponent();
+        TitleBar.Height = 0;
+        TitleBar.ExtendsContentIntoTitleBar = true;
     }
 }
