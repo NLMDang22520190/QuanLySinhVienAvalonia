27f1483efe905ab3b8e0dcaaef6737d52818f697
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-03-24 16:08:31 +0700

    Updated ViewResultViewModel and ViewResultView for real-time date/time
    Updated the `ViewResultViewModel` class to include a new private string variable `_currentTime` and a public property `CurrentTime` that gets and sets `_currentTime` using the `RaiseAndSetIfChanged` method from the ReactiveUI library. Added a constructor for `ViewResultViewModel` that calls the `UpdateCurrentTime` method when a new instance of the class is created. The `UpdateCurrentTime` method is an asynchronous method that continuously updates the `CurrentTime` property every second with the current date and time in the format "HH:mm dd/MM/yyyy". In the `ViewResultView.axaml` file, the hardcoded text displaying the date and time has been replaced with a data binding to the `CurrentTime` property, allowing the displayed date and time to update in real time.

--- QuanLySinhVien/ViewModels/MainScreen/ViewResultViewModel.cs
+++ QuanLySinhVien/ViewModels/MainScreen/ViewResultViewModel.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Globalization;
+using System.Threading;
 using System.Threading.Tasks;
+using ReactiveUI;
 
 namespace QuanLySinhVien.ViewModels.MainScreen
 {
-    public class ViewResultViewModel :ViewModelBase
+    public class ViewResultViewModel : ViewModelBase
     {
+        private string _currentTime;
+
+        public String CurrentTime
+        {
+            get => _currentTime;
+            set => this.RaiseAndSetIfChanged(ref _currentTime, value);
+        }
+        public ViewResultViewModel()
+        {
+            // Khởi tạo và bắt đầu cập nhật thời gian
+            UpdateCurrentTime();
+        }
+
+        private async void UpdateCurrentTime()
+        {
+            while (true)
+            {
+                // Cập nhật thời gian mỗi giây
+                CurrentTime = DateTime.Now.ToString("HH:mm dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                // Chờ 1 giây trước khi cập nhật lại
+                await Task.Delay(1000);
+            }
+        }
     }
 }
