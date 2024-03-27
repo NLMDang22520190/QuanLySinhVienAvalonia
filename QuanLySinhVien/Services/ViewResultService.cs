f2aa4e19039ce51407a84da111c4fef46be265fd
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-03-27 14:47:44 +0700

    Summary: Refactored code and added new features in ViewResult
    This commit includes a series of changes across multiple files. In `QuanLySinhVien.csproj`, the reference to the `Models` folder was removed. `MainScreenViewModel.cs` saw the addition of the `System.Diagnostics` namespace, changes to `_viewModelList`, and modifications to the `CreateInstance` method.
    
    `ViewResultViewModel.cs` had several new namespaces, properties, and methods added, including undo/redo functionality and a service for getting results. The layout and styling of `ViewResultView.axaml` were updated, with new elements and bindings added.
    
    `ViewResultView.axaml.cs` had new namespaces added, a `ViewResultViewModel` field added, and modifications to the constructor and event handling. A new `ResultModel` class was added in `ResultModel.cs`, and a new `ViewResultService` class was added in `ViewResultService.cs` with a `GetResults` property.

---
+++ QuanLySinhVien/Services/ViewResultService.cs
@@ -0,0 +1,14 @@
+﻿using QuanLySinhVien.Models;
+using System.Collections.Generic;
+
+namespace QuanLySinhVien.Services
+{
+    public class ViewResultService
+    {
+        public IEnumerable<ResultModel> GetResults => new[]{
+        new ResultModel { ResultID = "1", ResultName = "Result 1" },
+        new ResultModel { ResultID = "2", ResultName = "Result 2" },
+        new ResultModel { ResultID = "3", ResultName = "Result 3" },
+        };
+    }
+}
