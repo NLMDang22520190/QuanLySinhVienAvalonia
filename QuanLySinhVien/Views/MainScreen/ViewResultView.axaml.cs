4d3a0bbeea9f0cac7564d95883f3295e4f0eef03
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-03-27 15:26:57 +0700

    Updated ViewModel and View for Undo/Redo functionality and List update
    Updated `ViewResultViewModel.cs` and `ViewResultView.axaml.cs` files. Added `UpdateCurrentTime` method to `ViewResultViewModel.cs` for updating current time every second, later removed it. Commented out `CanUndo` and `CanRedo` properties and replaced them with `canUndo` and `canRedo` methods for determining undo/redo operations. Updated `UndoCommand` and `RedoCommand` to use these new methods. Modified `UpdateListAsync` method to accept `ObservableCollection<ResultModel>` parameter and backup data before clearing `ListModels`. Added additional `using` statements in `ViewResultView.axaml.cs` for `System.Collections`, `System.Collections.Generic`, `QuanLySinhVien.Models`. Updated `AttachHandler` method to call `UpdateListAsync` with `modifiedList` parameter created from `DataGrid`'s `ItemsSource`.

--- QuanLySinhVien/Views/MainScreen/ViewResultView.axaml.cs
+++ QuanLySinhVien/Views/MainScreen/ViewResultView.axaml.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Reactive.Linq;
 using Avalonia.Controls;
 using HarfBuzzSharp;
+using QuanLySinhVien.Models;
 using QuanLySinhVien.ViewModels;
 using QuanLySinhVien.ViewModels.MainScreen;
 
@@ -20,19 +23,24 @@ namespace QuanLySinhVien.Views.MainScreen
             viewModel = (ViewResultViewModel)MainScreenViewModel.CreateInstance<ViewModelBase>(
                 MainScreenViewModel.ViewModelList, viewModelName);
 
+            AttachHandler();
+
         }
 
         private void AttachHandler()
         {
             if (viewModel != null)
             {
+                Debug.WriteLine("viewmodel not null");
                 var dataGrid = this.FindControl<DataGrid>("DataGrid"); // Thay "YourDataGridName" bằng tên thật của DataGrid của bạn
                 if (dataGrid != null)
                 {
+                    Debug.WriteLine("datagrid not null");
                     dataGrid.CellEditEnded += async (sender, e) =>
                     {
                         Debug.WriteLine("ended");
-                        await viewModel.UpdateListCommand.Execute();
+                        var modifiedList = new ObservableCollection<ResultModel>((IEnumerable<ResultModel>)dataGrid.ItemsSource);
+                        await viewModel.UpdateListAsync(modifiedList);
                     };
                 }
             }
