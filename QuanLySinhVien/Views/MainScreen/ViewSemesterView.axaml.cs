279efee3c94975d0a6fca1b8838b3aaf9c216f82
Admin <Admin@DESKTOP-JPS8CQ9>
2024-03-30 15:03:17 +0700

    create NamHoc

---
+++ QuanLySinhVien/Views/MainScreen/ViewSemesterView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace QuanLySinhVien.Views.MainScreen
+{
+    public partial class ViewSemesterView : UserControl
+    {
+        public ViewSemesterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
