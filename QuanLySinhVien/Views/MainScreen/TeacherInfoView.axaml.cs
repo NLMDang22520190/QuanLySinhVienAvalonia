c2d2fb32c1d48636acf0aaec9f0075c0bd92199f
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-04-10 18:17:41 +0700

    Updated MainScreenView and added TeacherInfoView
    Updated `MainScreenView.axaml` to modify `NavigationViewItem` properties. Added a new namespace `QuanLySinhVien.ViewModels.MainScreen` in `TeacherInfoViewModel.cs` and created a new class `TeacherInfoViewModel` that inherits from `ViewModelBase`. Created a new `UserControl` in `TeacherInfoView.axaml` with various namespaces defined and content set to "Welcome to Avalonia!". Also, a new partial class `TeacherInfoView` has been created in `TeacherInfoView.axaml.cs` that inherits from `UserControl` with a constructor calling `InitializeComponent()`.

---
+++ QuanLySinhVien/Views/MainScreen/TeacherInfoView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace QuanLySinhVien.Views.MainScreen
+{
+    public partial class TeacherInfoView : UserControl
+    {
+        public TeacherInfoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
