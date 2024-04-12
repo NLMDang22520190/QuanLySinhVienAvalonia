b0cac26c05f6c7027234d7350ded4c579e486eac
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-04-12 14:23:59 +0700

    Added new TeacherInfo feature and updated UI
    This commit includes several significant changes:
    - Added a new `StreamGeometry` icon in `Icons.axaml` for use in the application.
    - Created a new `ObservableCollection` in `TeacherInfoViewModel.cs` to store teacher information.
    - Added a new navigation item "Học lực" in `MainScreenView.axaml`.
    - Redesigned `TeacherInfoView.axaml` with a title, search bar, add new teacher button, and a `DataGrid` for displaying teacher list.
    - Introduced a new model class `TeacherInfoModel` in `TeacherInfoModel.cs` for storing teacher's details.

---
+++ QuanLySinhVien/Models/TeacherInfoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien.Models
+{
+    public class TeacherInfoModel
+    {
+        public string TeacherName { get; set; } = String.Empty;
+        public string TeacherBirthDay { get; set; } = String.Empty;
+
+        public string TeacherGender { get; set; } = String.Empty;
+
+        public string TeacherAddress { get; set; } = String.Empty;
+
+        public string TeacherEmail { get; set; } = String.Empty;
+    }
+}
--- QuanLySinhVien/ViewModels/MainScreen/TeacherInfoViewModel.cs
+++ QuanLySinhVien/ViewModels/MainScreen/TeacherInfoViewModel.cs
@@ -1,12 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuanLySinhVien.Models;
+using ReactiveUI;
 
 namespace QuanLySinhVien.ViewModels.MainScreen
 {
     public class TeacherInfoViewModel : ViewModelBase
     {
+        private ObservableCollection<TeacherInfoModel> listTeacherInfoModels;
+
+        public ObservableCollection<TeacherInfoModel> ListTeacherInfoModels
+        {
+            get => listTeacherInfoModels;
+            set => this.RaiseAndSetIfChanged(ref listTeacherInfoModels, value);
+        }
+
+        public TeacherInfoViewModel()
+        {
+            ListTeacherInfoModels = new ObservableCollection<TeacherInfoModel>();
+            ListTeacherInfoModels.Add(new TeacherInfoModel
+            {
+                TeacherName = "Nguyen Van A",
+                TeacherBirthDay = "01/01/1990",
+                TeacherGender = "Nam",
+                TeacherAddress = "Ha Noi",
+                TeacherEmail = ""
+            });
+
+            // Add 4 more TeacherInfoModel objects to the list
+            ListTeacherInfoModels.Add(new TeacherInfoModel
+            {
+                TeacherName = "Nguyen Van B",
+                TeacherBirthDay = "02/02/1991",
+                TeacherGender = "Nam",
+                TeacherAddress = "Ho Chi Minh",
+                TeacherEmail = ""
+            });
+
+            ListTeacherInfoModels.Add(new TeacherInfoModel
+            {
+                TeacherName = "Nguyen Thi C",
+                TeacherBirthDay = "03/03/1992",
+                TeacherGender = "Nu",
+                TeacherAddress = "Da Nang",
+                TeacherEmail = ""
+            });
+
+            ListTeacherInfoModels.Add(new TeacherInfoModel
+            {
+                TeacherName = "Tran Van D",
+                TeacherBirthDay = "04/04/1993",
+                TeacherGender = "Nam",
+                TeacherAddress = "Hue",
+                TeacherEmail = ""
+            });
+
+            ListTeacherInfoModels.Add(new TeacherInfoModel
+            {
+                TeacherName = "Le Thi E",
+                TeacherBirthDay = "05/05/1994",
+                TeacherGender = "Nu",
+                TeacherAddress = "Can Tho",
+                TeacherEmail = ""
+            });
+        }
     }
 }
