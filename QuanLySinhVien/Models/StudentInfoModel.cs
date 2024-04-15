bad39e58af4faf71b29de8a3c72e4619e4d2c651
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-04-15 09:00:04 +0700

    Added ReactiveUI and student info features to StudentInfoViewModel
    In `StudentInfoViewModel.cs`, ReactiveUI library was added for reactive programming. A new property `CurrentTime` was added to display the current time, along with an asynchronous method `UpdateCurrentTime` to update it every second. An `ObservableCollection` of `StudentInfoModel` named `ListStudentInfoModels` was added to store the list of students. The constructor was updated to initialize `ListStudentInfoModels` and add five `StudentInfoModel` objects to the list, and to call `UpdateCurrentTime` method.
    
    In `StudentInfoView.axaml`, the view was updated with a white background, two new buttons for showing all students and adding a teacher, and adjusted margin of the search box. The `TextBlock` for displaying the current time was un-commented and bound to `CurrentTime` property. The old buttons for adding a teacher and showing all students were removed. The `DataGrid` was un-commented and updated to display student information instead of teacher information, with two new columns added for displaying the GPA of students for two semesters. The `DataGridTemplateColumn` was adjusted to modify the student information.
    
    In `StudentInfoModel.cs`, a new model class `StudentInfoModel` was created with properties for storing student information such as name, birthday, gender, address, email, and GPA for two semesters.

---
+++ QuanLySinhVien/Models/StudentInfoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien.Models
+{
+    public class StudentInfoModel
+    {
+        public string StudentName { get; set; }
+        public string StudentBirthDay { get; set; }
+        public string StudentGender { get; set; }
+        public string StudentAddress { get; set; }
+        public string StudentEmail { get; set; }
+
+        public float StudentGPA1 { get; set; }
+
+        public float StudentGPA2 { get; set; }
+    }
+}
