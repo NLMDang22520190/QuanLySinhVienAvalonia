981d92922a33ef93cf06319d066e624c47cc5cc0
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-04-15 09:00:04 +0700

    Added ReactiveUI and student info features to StudentInfoViewModel
    In `StudentInfoViewModel.cs`, ReactiveUI library was added for reactive programming. A new property `CurrentTime` was added to display the current time, along with an asynchronous method `UpdateCurrentTime` to update it every second. An `ObservableCollection` of `StudentInfoModel` named `ListStudentInfoModels` was added to store the list of students. The constructor was updated to initialize `ListStudentInfoModels` and add five `StudentInfoModel` objects to the list, and to call `UpdateCurrentTime` method.
    
    In `StudentInfoView.axaml`, the view was updated with a white background, two new buttons for showing all students and adding a teacher, and adjusted margin of the search box. The `TextBlock` for displaying the current time was un-commented and bound to `CurrentTime` property. The old buttons for adding a teacher and showing all students were removed. The `DataGrid` was un-commented and updated to display student information instead of teacher information, with two new columns added for displaying the GPA of students for two semesters. The `DataGridTemplateColumn` was adjusted to modify the student information.
    
    In `StudentInfoModel.cs`, a new model class `StudentInfoModel` was created with properties for storing student information such as name, birthday, gender, address, email, and GPA for two semesters.

--- QuanLySinhVien/ViewModels/MainScreen/StudentInfoViewModel.cs
+++ QuanLySinhVien/ViewModels/MainScreen/StudentInfoViewModel.cs
@@ -1,12 +1,107 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuanLySinhVien.Models;
 
 namespace QuanLySinhVien.ViewModels.MainScreen
 {
     public class StudentInfoViewModel : ViewModelBase
     {
+        #region Time
+
+        private string _currentTime;
+
+        public string CurrentTime
+        {
+            get => _currentTime;
+            set => this.RaiseAndSetIfChanged(ref _currentTime, value);
+        }
+
+        private async void UpdateCurrentTime()
+        {
+            while (true)
+            {
+                CurrentTime = DateTime.Now.ToString("HH:mm dd/MM/yy", CultureInfo.InvariantCulture);
+                await Task.Delay(1000);
+            }
+        }
+
+        #endregion Time
+
+        private ObservableCollection<StudentInfoModel> listStudentInfoModels;
+
+        public ObservableCollection<StudentInfoModel> ListStudentInfoModels
+        {
+            get => listStudentInfoModels;
+            set => this.RaiseAndSetIfChanged(ref listStudentInfoModels, value);
+        }
+
+        public StudentInfoViewModel()
+        {
+            ListStudentInfoModels = new ObservableCollection<StudentInfoModel>();
+            ListStudentInfoModels.Add(new StudentInfoModel
+            {
+                StudentName = "Nguyen Van A",
+                StudentBirthDay = "01/01/1990",
+                StudentGender = "Nam",
+                StudentAddress = "Ha Noi",
+                StudentEmail = "",
+                StudentGPA1 = 3.5f,
+                StudentGPA2 = 3.6f
+            });
+
+            // Add 4 more StudentInfoModel objects to the list
+            ListStudentInfoModels.Add(new StudentInfoModel
+            {
+                StudentName = "Nguyen Van B",
+                StudentBirthDay = "02/02/1991",
+                StudentGender = "Nam",
+                StudentAddress = "Ha Noi",
+                StudentEmail = "",
+                StudentGPA1 = 3.7f,
+                StudentGPA2 = 3.8f
+            });
+
+            ListStudentInfoModels.Add(new StudentInfoModel
+            {
+                StudentName = "Nguyen Van C",
+                StudentBirthDay = "03/03/1992",
+                StudentGender = "Nam",
+                StudentAddress = "Ha Noi",
+                StudentEmail = "",
+                StudentGPA1 = 3.9f,
+                StudentGPA2 = 4.0f
+            });
+
+            ListStudentInfoModels.Add(new StudentInfoModel
+            {
+                StudentName = "Nguyen Van D",
+                StudentBirthDay = "04/04/1993",
+                StudentGender = "Nam",
+                StudentAddress = "Ha Noi",
+                StudentEmail = "",
+                StudentGPA1 = 4.1f,
+                StudentGPA2 = 4.2f
+            });
+
+            ListStudentInfoModels.Add(new StudentInfoModel
+            {
+                StudentName = "Nguyen Van E",
+                StudentBirthDay = "05/05/1994",
+                StudentGender = "Nam",
+                StudentAddress = "Ha Noi",
+                StudentEmail = "",
+                StudentGPA1 = 4.3f,
+                StudentGPA2 = 4.4f
+            });
+
+            UpdateCurrentTime();
+        
+        }
     }
 }
