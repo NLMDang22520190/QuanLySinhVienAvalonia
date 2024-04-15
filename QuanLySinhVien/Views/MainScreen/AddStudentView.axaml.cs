9d6552143854e8ba281b9874f4a1ab138c4d351c
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-04-15 09:12:25 +0700

    Add AddStudentView and related ViewModel
    A new `AddStudentViewModel.cs` file was added, containing an empty `AddStudentViewModel` class for future logic handling. A new Avalonia UI file, `AddStudentView.axaml`, was also added, featuring a detailed layout for the `AddStudentView` window, including a grid, dock panel, and various input fields and buttons with custom styles. Additionally, a new `AddStudentView.axaml.cs` file was created, containing a partial `AddStudentView` class with initialized UI components and `TitleBar` properties.

---
+++ QuanLySinhVien/Views/MainScreen/AddStudentView.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia.Controls;
+using FluentAvalonia.UI.Windowing;
+
+namespace QuanLySinhVien.Views.MainScreen
+{
+    public partial class AddStudentView : AppWindow
+    {
+
+        public AddStudentView()
+        {
+            InitializeComponent();
+            TitleBar.Height = -1;
+            TitleBar.ExtendsContentIntoTitleBar = true;
+        }
+    }
+}
