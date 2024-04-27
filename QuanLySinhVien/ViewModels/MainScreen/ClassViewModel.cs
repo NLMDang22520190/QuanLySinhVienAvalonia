ec0009c02383ff7bf4745d2ee9ac7bb302856d25
22520187 <146148909+22520187@users.noreply.github.com>
2024-04-27 23:28:24 +0700

    UI Class + ClassDetail + AddClass

---
+++ QuanLySinhVien/ViewModels/MainScreen/ClassViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLySinhVien.Models;
+using ReactiveUI;
+
+namespace QuanLySinhVien.ViewModels.MainScreen
+{
+    public class ClassViewModel: ViewModelBase
+    {
+    }
+}
---
+++ QuanLySinhVien/Views/MainScreen/AddClassView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace QuanLySinhVien.Views.MainScreen
+{
+    public partial class AddClassView : UserControl
+    {
+        public AddClassView()
+        {
+            InitializeComponent();
+        }
+    }
+}
