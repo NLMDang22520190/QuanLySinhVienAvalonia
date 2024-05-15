396b4cc5bba839d543445fbb54a6e82b68d5cab3
22520187 <146148909+22520187@users.noreply.github.com>
2024-05-15 22:56:49 +0700

    Add student to class

---
+++ QuanLySinhVien/Views/MainScreen/AddStudentToClassView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace QuanLySinhVien.Views.MainScreen
+{
+    public partial class AddStudentToClassView : UserControl
+    {
+        public AddStudentToClassView()
+        {
+            InitializeComponent();
+        }
+    }
+}
