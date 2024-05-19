e066817feef51fb2b2be920b223bdba957810659
NHBach-22520091 <22520091@gm.uit.edu.vn>
2024-05-19 12:07:35 +0700

    Report semester view + Report subject view

---
+++ QuanLySinhVien/Views/MainScreen/ReportSemesterView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace QuanLySinhVien.Views.MainScreen
+{
+    public partial class ReportSemesterView : UserControl
+    {
+        public ReportSemesterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
