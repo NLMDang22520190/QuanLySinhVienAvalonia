278cbc5fe40cfcfb0791fae3af7041ae116b1f1d
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-06-06 22:13:12 +0700

    Add functionality to add new teacher in system
    This commit includes updates to the `AddTeacherViewModel.cs`, `AddTeacherView.axaml`, and `AddTeacherView.axaml.cs` files. In `AddTeacherViewModel.cs`, properties (`TenGiaoVien`, `NgaySinh`, `GioiTinh`, `DiaChi`, `Email`) and methods (`OnAdd`, `OnCancel`) have been added to handle the addition of a new teacher. A `AddCommand` ReactiveCommand has been created to execute the `OnAdd` method when invoked with valid inputs. In `AddTeacherView.axaml`, bindings have been added to the text fields and commands, and the `GioiTinh` ComboBox has been updated. The `AddTeacherView.axaml.cs` file now handles selection changes for the `GioiTinh` ComboBox and the `NgaySinh` CalendarDatePicker, setting the selected values to the respective properties in the `AddTeacherViewModel`.

--- QuanLySinhVien/ViewModels/MainScreen/AddTeacherViewModel.cs
+++ QuanLySinhVien/ViewModels/MainScreen/AddTeacherViewModel.cs
@@ -1,12 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
+using Avalonia.Controls;
+using QuanLySinhVien.Models;
+using ReactiveUI;
 
 namespace QuanLySinhVien.ViewModels.MainScreen
 {
     public class AddTeacherViewModel : ViewModelBase
     {
+        #region Properties
+
+        private string _tenGiaoVien = string.Empty;
+
+        public string TenGiaoVien
+        {
+            get => _tenGiaoVien;
+            set => this.RaiseAndSetIfChanged(ref _tenGiaoVien, value);
+        }
+
+        private DateTime _ngaySinh;
+
+        public DateTime NgaySinh
+        {
+            get => _ngaySinh;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _ngaySinh, value);
+                Debug.WriteLine(_ngaySinh);
+            }
+        }
+
+        private bool? _gioiTinh;
+
+        public bool? GioiTinh
+        {
+            get => _gioiTinh;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _gioiTinh, value);
+            }
+        }
+
+        private string _diaChi = string.Empty;
+
+        public string DiaChi
+        {
+            get => _diaChi;
+            set => this.RaiseAndSetIfChanged(ref _diaChi, value);
+        }
+
+        private string _email = string.Empty;
+
+        public string Email
+        {
+            get => _email;
+            set => this.RaiseAndSetIfChanged(ref _email, value);
+        }
+
+
+        #endregion
+
+        public ReactiveCommand<Window, Unit> AddCommand { get; }
+
+        public AddTeacherViewModel()
+        {
+            var isValidObservable = this.WhenAnyValue(
+                x => x.TenGiaoVien,
+                x => x.DiaChi,
+                x => x.Email,
+                x => x.NgaySinh,
+                x => x.GioiTinh,
+                (tenGiaoVien, diaChi, email, ngaySinh, gioiTinh) =>
+                {
+                    return !string.IsNullOrWhiteSpace(tenGiaoVien)
+                           && !string.IsNullOrWhiteSpace(diaChi)
+                           && !string.IsNullOrWhiteSpace(email)
+                           && ngaySinh != DateTime.MinValue
+                           && gioiTinh.HasValue;
+                });
+
+            AddCommand = ReactiveCommand.Create<Window>(OnAdd, isValidObservable);
+
+        }
+
+        private void OnAdd(Window window)
+        {
+            var newGiaoVien = new GiaoVien
+            {
+                MaGiaoVien = "GV" + (DataProvider.Ins.DB.GiaoViens.Count() + 1).ToString(),
+                TenGiaoVien = TenGiaoVien,
+                NgaySinh = NgaySinh,
+                GioiTinh = GioiTinh,
+                DiaChi = DiaChi,
+                Email = Email
+            };
+            DataProvider.Ins.DB.GiaoViens.Add(newGiaoVien);
+            DataProvider.Ins.DB.SaveChanges();
+            window.Close();
+
+        }
+
+        private void OnCancel(Window window)
+        {
+            window.Close();
+        }
     }
 }
--- QuanLySinhVien/Views/MainScreen/AddTeacherView.axaml.cs
+++ QuanLySinhVien/Views/MainScreen/AddTeacherView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using FluentAvalonia.UI.Windowing;
+using QuanLySinhVien.ViewModels.MainScreen;
 
 namespace QuanLySinhVien.Views.MainScreen
 {
@@ -11,6 +12,39 @@ namespace QuanLySinhVien.Views.MainScreen
             TitleBar.Height = -1;
             TitleBar.ExtendsContentIntoTitleBar = true;
             this.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            var gioiTinhComboBox = this.FindControl<ComboBox>("GioiTinh");
+            if (gioiTinhComboBox != null)
+            {
+                gioiTinhComboBox.SelectionChanged += (sender, args) =>
+                {
+                    var comboBox = (ComboBox)sender;
+                    var selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+                    var content = selectedItem.Content.ToString();
+                    if (content == "Nam")
+                    {
+                        ((AddTeacherViewModel)this.DataContext).GioiTinh = true;
+                    }
+                    else
+                    {
+                        ((AddTeacherViewModel)this.DataContext).GioiTinh = false;
+                    }
+                };
+            }
+
+            var ngaySinhCalenderDatePicker = this.FindControl<CalendarDatePicker>("NgaySinh");
+            if (ngaySinhCalenderDatePicker != null)
+            {
+                ngaySinhCalenderDatePicker.SelectedDateChanged += (sender, args) =>
+                {
+                    var datePicker = (CalendarDatePicker)sender;
+                    var selectedDate = datePicker.SelectedDate;
+                    if (selectedDate.HasValue)
+                    {
+                        ((AddTeacherViewModel)this.DataContext).NgaySinh = selectedDate.Value;
+                    }
+                };
+            }
         }
     }
 }
