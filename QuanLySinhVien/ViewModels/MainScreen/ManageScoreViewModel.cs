0d2208990a5e2b20046fbe614b3ae411ec8110c4
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-06-06 08:42:53 +0700

    Refactor ManageScoreViewModel for centralized DB access
    Refactored the `ManageScoreViewModel` class to remove direct database context management. The `QlhsContext db` field and its instantiation in the constructor were removed. The database context in the `LoadDataAsync` method was updated to access through the `DataProvider` singleton, improving the safety and centralization of database access. Additionally, the `Microsoft.EntityFrameworkCore` namespace was added to enable the use of Entity Framework Core functionalities.

--- QuanLySinhVien/ViewModels/MainScreen/ManageScoreViewModel.cs
+++ QuanLySinhVien/ViewModels/MainScreen/ManageScoreViewModel.cs
@@ -11,7 +11,6 @@ namespace QuanLySinhVien.ViewModels.MainScreen
     public class ManageScoreViewModel : ViewModelBase
     {
         private ObservableCollection<string> nienKhoas;
-        private readonly QlhsContext db;
 
         public ObservableCollection<string> NienKhoas
         {
@@ -21,7 +20,6 @@ namespace QuanLySinhVien.ViewModels.MainScreen
 
         public ManageScoreViewModel()
         {
-            db = new QlhsContext();
             LoadDataAsync();
         }
 
@@ -29,7 +27,7 @@ namespace QuanLySinhVien.ViewModels.MainScreen
         {
             try
             {
-                var result = await db.NienKhoas.Select(nk => nk.TenNienKhoa).ToListAsync();
+                var result = await DataProvider.Ins.DB.NienKhoas.Select(nk => nk.TenNienKhoa).ToListAsync();
                 NienKhoas = new ObservableCollection<string>(result);
             }
             catch (Exception ex)
