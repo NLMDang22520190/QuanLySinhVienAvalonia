793d7d6aa9c7f0d364ec5d6390af68645d05a5df
Mai Thanh Bách <22520090@gm.uit.edu.vn>
2024-06-11 23:52:49 +0700

    Added new ViewStudentsAccomplishment UI and ViewModel
    A new ViewModel `ViewStudentsAccomplishmentViewModel.cs` and corresponding Avalonia UI `ViewStudentsAccomplishmentView.axaml` were added. The ViewModel is currently empty. The UI includes a `UserControl` with a `Grid` layout, sorting tools, and a `DataGrid` for displaying student data. A new partial class `ViewStudentsAccomplishmentView.axaml.cs` was also added to handle UI logic.

---
+++ QuanLySinhVien/Views/MainScreen/ViewStudentsAccomplishmentView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace QuanLySinhVien.Views.MainScreen
+{
+    public partial class ViewStudentsAccomplishmentView : UserControl
+    {
+        public ViewStudentsAccomplishmentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
