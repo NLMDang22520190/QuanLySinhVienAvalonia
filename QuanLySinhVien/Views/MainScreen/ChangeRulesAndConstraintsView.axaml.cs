6938d06eb2e68289506719599342884188e8e410
Mai Thanh Bách <22520090@gm.uit.edu.vn>
2024-06-11 23:21:06 +0700

    Add ChangeRulesAndConstraintsView and ViewModel
    A new ViewModel class `ChangeRulesAndConstraintsViewModel` has been added to the `QuanLySinhVien.ViewModels.MainScreen` namespace. A new Avalonia UserControl `ChangeRulesAndConstraintsView` has been added to allow users to change rules and constraints. The view contains a main grid with three rows and two columns for rule selection and editing. The code-behind file for the UserControl has also been added.

---
+++ QuanLySinhVien/Views/MainScreen/ChangeRulesAndConstraintsView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace QuanLySinhVien.Views.MainScreen
+{
+    public partial class ChangeRulesAndConstraintsView : UserControl
+    {
+        public ChangeRulesAndConstraintsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
