f31e2a38cc18153d8e9f6e4487c15d87c6197686
Mai Thanh Bách <22520090@gm.uit.edu.vn>
2024-06-12 18:50:57 +0700

    Added new properties, commands, and UI changes in ChangeRulesAndConstraints
    In `ChangeRulesAndConstraintsViewModel.cs`, new properties (`MaQuiDinh`, `TenQuiDinh`, `GiaTri`) and commands (`SaveCommand`, `CancelCommand`) were added. The `Save` and `Cancel` methods were also implemented, but are currently empty.
    
    In `ChangeRulesAndConstraintsView.axaml`, the layout was significantly modified. The grid column definitions were adjusted, and the layout was split into two columns, each containing a border with a text block and a text box.
    
    The "Save" button was removed, and tooltips were added to the "Cancel" and "Confirm" buttons to provide additional information to the user. These changes aim to improve the user interface and add functionality for saving and canceling changes.

--- QuanLySinhVien/ViewModels/MainScreen/ChangeRulesAndConstraintsViewModel.cs
+++ QuanLySinhVien/ViewModels/MainScreen/ChangeRulesAndConstraintsViewModel.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using Avalonia.Controls;
+using QuanLySinhVien.Models;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace QuanLySinhVien.ViewModels.MainScreen
 {
     public class ChangeRulesAndConstraintsViewModel : ViewModelBase
     {
+        #region Model's properties
+        private string _maQuiDinh = string.Empty;
+        public string MaQuiDinh
+        {
+            get => _maQuiDinh;
+            set => this.RaiseAndSetIfChanged(ref _maQuiDinh, value);
+        }
+
+        private string _tenQuiDinh = string.Empty;
+
+        public string TenQuiDinh
+        {
+            get => _tenQuiDinh;
+            set => this.RaiseAndSetIfChanged(ref _tenQuiDinh, value);
+        }
+
+        private int? _giaTri = 0;
+
+        public int? GiaTri
+        {
+            get => _giaTri;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _giaTri, value);
+            }
+        }
+
+        #endregion
+
+
+        #region Command
+        public ReactiveCommand<Unit, Unit> SaveCommand { get; }
+        public ReactiveCommand<Unit, Unit> CancelCommand { get; }
+
+        #endregion
+
+        public ChangeRulesAndConstraintsViewModel()
+        {
+            SaveCommand = ReactiveCommand.Create(Save);
+            CancelCommand = ReactiveCommand.Create(Cancel);
+        }
+
+        private void Save()
+        {
+            // Save changes to database
+        }
+
+        private void Cancel()
+        {  
+            // Reset to previous state
+        }
     }
 }
