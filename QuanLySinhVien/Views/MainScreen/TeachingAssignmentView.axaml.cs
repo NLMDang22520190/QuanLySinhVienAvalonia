7134dacfaf9154f23327f5408fba41a90382b235
bdquoc <22520098@gm.uit.edu.vn>
2024-06-12 21:36:26 +0700

    ui subject and teachingassignment

---
+++ QuanLySinhVien/Views/MainScreen/TeachingAssignmentView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace QuanLySinhVien.Views.MainScreen
+{
+    public partial class TeachingAssignmentView : UserControl
+    {
+        public TeachingAssignmentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
