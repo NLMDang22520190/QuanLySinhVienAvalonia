94dab3d234f70866000a6d2130da178b57125492
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-06-12 22:11:32 +0700

    Update user info management and image resources
    - Removed references to outdated images in QuanLySinhVien.csproj.
    - Updated ForgotPasswordView.axaml to use `ForgotPass.png`.
    - Added `Tag` attribute to a NavigationViewItem in MainScreenView.axaml.
    - Introduced `UserInfoViewModel` for user information management.
    - Created `UserInfoView` in Avalonia UI for displaying user info.
    - Implemented code-behind for `UserInfoView` to link UI and logic.

---
+++ QuanLySinhVien/Views/MainScreen/UserInfoView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace QuanLySinhVien.Views.MainScreen
+{
+    public partial class UserInfoView : UserControl
+    {
+        public UserInfoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
