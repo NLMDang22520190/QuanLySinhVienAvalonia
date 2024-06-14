cfb718d0c4559e05c7bba7bf09eb19270a905f08
bdquoc <22520098@gm.uit.edu.vn>
2024-06-14 23:44:03 +0700

    fix searchSubject AddAssignment

--- QuanLySinhVien/Views/MainScreen/TeachingAssignmentView.axaml.cs
+++ QuanLySinhVien/Views/MainScreen/TeachingAssignmentView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using QuanLySinhVien.ViewModels.MainScreen;
 
 namespace QuanLySinhVien.Views.MainScreen
 {
@@ -7,6 +8,32 @@ namespace QuanLySinhVien.Views.MainScreen
         public TeachingAssignmentView()
         {
             InitializeComponent();
+            SetUp();
+        }
+        private void SetUp()
+        {
+
+            var SearchTextBox = this.FindControl<TextBox>("SearchTextBox");
+            if (SearchTextBox != null)
+            {
+                SearchTextBox.TextChanged += (sender, args) =>
+                {
+                    var textBox = (TextBox)sender;
+                    var text = textBox.Text;
+                    ((TeachingAssignmentViewModel)this.DataContext).SearchSubject(text);
+                };
+            }
+
+            var DataGrid = this.FindControl<DataGrid>("DataGrid");
+            if (DataGrid != null)
+            {
+                DataGrid.SelectionChanged += (sender, args) =>
+                {
+                    var dataGrid = (DataGrid)sender;
+                    var selectedPhanCongIndex = dataGrid.SelectedIndex;
+                    ((TeachingAssignmentViewModel)this.DataContext).SelectedPhanCongIndex = selectedPhanCongIndex;
+                };
+            }
         }
     }
 }
