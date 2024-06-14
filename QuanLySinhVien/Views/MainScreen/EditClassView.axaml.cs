b1927aafea56b310b8f257ac411170c19e123a16
22520187 <146148909+22520187@users.noreply.github.com>
2024-06-14 10:17:43 +0700

    fix UI Class and Add Tittle

--- QuanLySinhVien/Views/MainScreen/EditClassView.axaml.cs
+++ QuanLySinhVien/Views/MainScreen/EditClassView.axaml.cs
@@ -1,12 +1,19 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Chrome;
+using FluentAvalonia.UI.Windowing;
+using QuanLySinhVien.ViewModels.MainScreen;
 
 namespace QuanLySinhVien.Views.MainScreen
 {
-    public partial class EditClassView : Window
+    public partial class EditClassView : AppWindow
     {
         public EditClassView()
         {
             InitializeComponent();
+            TitleBar.Height = -1;
+            TitleBar.ExtendsContentIntoTitleBar = true;
+            this.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
         }
     }
 }
